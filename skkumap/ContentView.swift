0b4bcf57562e3f2938328671868ce006aa4ea737
spencer0124 <spencer0124@naver.com>
2023-12-21 17:36:16 +0900

    Initial Commit

---
+++ skkumap/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  skkumap
+//
+//  Created by 조승용 on 2023/12/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ skkumap/skkumapApp.swift
@@ -0,0 +1,17 @@
+//
+//  skkumapApp.swift
+//  skkumap
+//
+//  Created by 조승용 on 2023/12/21.
+//
+
+import SwiftUI
+
+@main
+struct skkumapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
