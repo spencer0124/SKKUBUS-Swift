0f8d8cc1a65547251c4ae2dae523ea7fd522e518
spencer0124 <spencer0124@naver.com>
2024-05-17 13:27:55 +0900

    add: navigation stack for sublaunchscreen

--- skkumap/Feature/SubLaunchScreen/SubLaunchScreen.swift
+++ skkumap/Feature/SubLaunchScreen/SubLaunchScreen.swift
@@ -16,70 +16,78 @@ extension Color {
 struct SubLaunchScreen: View {
     
     @State private var animateGradient: Bool = false
+    @State private var navigateToNextView: Bool = false
     
     var body: some View {
-        VStack {
-            Spacer()
-            HStack {
+        NavigationStack {
+            VStack {
                 Spacer()
-                VStack(spacing:0) {
-                    Image("skku_logo_black")
-                        .resizable()
-                        .renderingMode(.template)
-                        .frame(width: 150, height: 150)
-                        .foregroundColor(.white)
-                    
-                    Image("skku_text")
-                        .resizable()
-                        .renderingMode(.template)
-                        .scaledToFit()
-                        .frame(height: 50)
-                        .foregroundColor(.white)
+                HStack {
+                    Spacer()
+                    VStack(spacing:0) {
+                        Image("skku_logo_black")
+                            .resizable()
+                            .renderingMode(.template)
+                            .frame(width: 150, height: 150)
+                            .foregroundColor(.white)
                         
-                    
+                        Image("skku_text")
+                            .resizable()
+                            .renderingMode(.template)
+                            .scaledToFit()
+                            .frame(height: 50)
+                            .foregroundColor(.white)
+                        
+                        
+                        
+                        Spacer()
+                            .frame(height: 270)
+                        
+                    }
                     
                     Spacer()
-                        .frame(height: 270)
-                    
                 }
                 
                 Spacer()
             }
-//            Spacer()
-//                .frame(height: 250)
-            Spacer()
-        }
-        .background {
-            ZStack {
-                Color.customDeepGreen1.edgesIgnoringSafeArea(.all)
-                
-                Rectangle()
-                    .fill(
-                        LinearGradient(
-                            gradient: Gradient(colors: [
-                                .green2.opacity(0.9),
-                                .green2.opacity(0.7),
-                                .green2.opacity(0.3),
-                                .green2.opacity(0.01),
-                                
-                                
-                                
-                            ]),
-                            startPoint: .top,
-                            endPoint: .bottom
+            .background {
+                ZStack {
+                    Color.customDeepGreen1.edgesIgnoringSafeArea(.all)
+                    
+                    Rectangle()
+                        .fill(
+                            LinearGradient(
+                                gradient: Gradient(colors: [
+                                    .green2.opacity(0.9),
+                                    .green2.opacity(0.7),
+                                    .green2.opacity(0.3),
+                                    .green2.opacity(0.01),
+                                    
+                                    
+                                    
+                                ]),
+                                startPoint: .top,
+                                endPoint: .bottom
+                            )
                         )
-                    )
-                    .frame(height: UIScreen.main.bounds.height * 1.2) // Extend the height a bit more to avoid the black line
-                    .offset(y: animateGradient ? 50 : -UIScreen.main.bounds.height * 1.2) // Start above the screen and animate down to 0
-                    .animation(Animation.easeInOut(duration: 1.3), value: animateGradient)
-                    .onAppear {
-                        animateGradient = true
-//                        DispatchQueue.main.asyncAfter(deadline: .now() + 1.5) {
-//                            withAnimation(Animation.easeInOut(duration: 1.5)) {
-//                                animateGradient = false
-//                            }
-//                        }
-                    }
+                        .frame(height: UIScreen.main.bounds.height * 1.2)
+                        .offset(y: animateGradient ? 50 : -UIScreen.main.bounds.height * 1.2)
+                        .animation(Animation.easeInOut(duration: 1.3), value: animateGradient)
+                        .onAppear {
+                            // 이걸로 감싸주지 않으면 Top left에서 애니메이션이 시작한다
+                            DispatchQueue.main.async {
+                                animateGradient = true
+                            }
+                            DispatchQueue.main.asyncAfter(deadline: .now() + 1.5) {
+//                                 1.3s for animation + 0.5s delay
+                                navigateToNextView = true
+                            }
+                        }
+                }
+            }
+            
+            .navigationDestination(isPresented: $navigateToNextView) {
+                MapView().navigationBarBackButtonHidden(true)
             }
         }
     }
